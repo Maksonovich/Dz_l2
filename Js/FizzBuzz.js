eada3f01ed47b99997b8056096884321584b98e6
Maksonovich <kaverron1@gmail.com>
2022-09-08 18:50:02 +0300

    Поменял в FizzBuzz (elseif) чтобы 100 была Buzz

--- Js/FizzBuzz.js
+++ Js/FizzBuzz.js
@@ -1,9 +1,9 @@
-for (let i = 1; i < 100; i++) {
+for (let i = 0; i < 101; i++) {
     if (i % 3 === 0) {
         console.log("Fizz");
     }else if (i % 5 === 0) {
         console.log("Buzz");
-    }else if (i % 3 === 0 && i % 5 === 0) {
+    }else if (i % 15 === 0) {
         console.log("fizzbuzz");
     }else {
         console.log(i); 
